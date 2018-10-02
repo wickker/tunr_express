950cbce008fbd10754af7f310f7b15e22ded4a3d
awongh <akira@awongh.com>
2018-10-02 13:00:34 +0800

    pool stuff

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ const methodOverride = require('method-override');
 const pg = require('pg');
 
 // Initialise postgres client
-const client = new pg({
+const configs = {
   user: 'YOURUSERNAME',
   host: '127.0.0.1',
   database: 'tunr_db',
   port: 5432,
+};
+
+const pool = new pg.Pool(configs);
+
+pool.on('error', function (err) {
+  console.log('idle client error', err.message, err.stack);
 });
 
 /**
@@ -61,3 +67,11 @@ app.get('/new', (request, response) => {
  * ===================================
  */
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+
+server.on('close', () => {
+  console.log('Closed express server');
+
+  db.pool.end(() => {
+    console.log('Shut down db connection pool');
+  });
+});
