634553acf2f9070bfae80aaabbd0c6c5c4ece4ee
adnacg <38314683+adnacg@users.noreply.github.com>
2018-10-02 13:00:34 +0800

    Create artist_data.sql

---
+++ artist_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO artists(name, photo_url, nationality) VALUES(Yeah Yeah Yeahs, http://www.athousandguitars.com/wp-content/uploads/2013/04/yeah-yeah-yeahs.jpg, USA);
+INSERT INTO artists(name, photo_url, nationality) VALUES(Nosaj Thing, http://wertn.com/wp-content/uploads/2012/04/Nosaj-Thing_Mondrian_CL_High-3487.jpg, USA);
+INSERT INTO artists(name, photo_url, nationality) VALUES(Norah Jones, http://entertainmentrealm.files.wordpress.com/2012/05/norahjones1.jpg, USA);
+INSERT INTO artists(name, photo_url, nationality) VALUES(Lykke Li, http://www.chartattack.com/wp-content/uploads/2012/07/lykke-li-newmain1-photo-by-daniel-jackson.jpg, Sweeden);
+INSERT INTO artists(name, photo_url, nationality) VALUES(Kendrick Lamar, http://www.xxlmag.com/wp-content/uploads/2013/06/kendricklamar_001-1600.jpg, USA);
